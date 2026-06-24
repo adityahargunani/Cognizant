e2aeade309d1d1970e1c39226d25125e50e6a05b
adityahargunani <aditya.hargunani.10@gmail.com>
2026-06-24 18:54:04 +0530

    Week 2

---
+++ Week 2/Spring Core and Maven/Exercise 2- Implementing Dependency Injection/librarymanagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void findAll() {
+        System.out.println("BookRepository: Finding all books in the database...");
+    }
+}
